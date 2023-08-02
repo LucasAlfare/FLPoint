7951fad2011d989285eef7bbf1c9b13b50541ebd
LucasAlfare <god-lucas@hotmail.com>
2023-08-01 21:45:41 -0300

    fix indentation

--- server/src/main/kotlin/com/lucasalfare/flpoint/server/service/MongoUsersService.kt
+++ server/src/main/kotlin/com/lucasalfare/flpoint/server/service/MongoUsersService.kt
@@ -13,42 +13,42 @@ import org.bson.types.ObjectId
 
 
 object MongoUsersService : DataCRUDAdapter<User, ObjectId>() {
-    private const val COLLECTION_NAME = "users"
+  private const val COLLECTION_NAME = "users"
 
-    private val usersCollection = MongoDbSetup
-        .db
-        .getCollection<User>(COLLECTION_NAME)
+  private val usersCollection = MongoDbSetup
+    .db
+    .getCollection<User>(COLLECTION_NAME)
 
-    override suspend fun create(next: User): Boolean {
-        if (getById(next.id!!) != null) return false
-        if (next.credentials == null) return false
-        if (next.credentials!!.username == null || next.credentials!!.password == null) return false
-        if (next.credentials!!.username!!.isEmpty() || next.credentials!!.password!!.isEmpty()) return false
+  override suspend fun create(next: User): Boolean {
+    if (getById(next.id!!) != null) return false
+    if (next.credentials == null) return false
+    if (next.credentials!!.username == null || next.credentials!!.password == null) return false
+    if (next.credentials!!.username!!.isEmpty() || next.credentials!!.password!!.isEmpty()) return false
 
-        next.credentials!!.hashPassword()
+    next.credentials!!.hashPassword()
 
-        return usersCollection.insertOne(next).wasAcknowledged()
-    }
+    return usersCollection.insertOne(next).wasAcknowledged()
+  }
 
-    override suspend fun getAll() =
-        usersCollection.find().toList()
+  override suspend fun getAll() =
+    usersCollection.find().toList()
 
-    override suspend fun getById(id: ObjectId) =
-        usersCollection.find(eq(User::id.name, id)).firstOrNull()
+  override suspend fun getById(id: ObjectId) =
+    usersCollection.find(eq(User::id.name, id)).firstOrNull()
 
-    override suspend fun updateById(id: ObjectId, nextValues: User): Boolean {
-        val filter = eq(User::id.name, id)
-        val update = combine(
-            set(User::maxAuthenticationsPerDay.name, nextValues.maxAuthenticationsPerDay)
-        )
+  override suspend fun updateById(id: ObjectId, nextValues: User): Boolean {
+    val filter = eq(User::id.name, id)
+    val update = combine(
+      set(User::maxAuthenticationsPerDay.name, nextValues.maxAuthenticationsPerDay)
+    )
 
-        return usersCollection.updateOne(filter, update).wasAcknowledged()
-    }
+    return usersCollection.updateOne(filter, update).wasAcknowledged()
+  }
 
-    override suspend fun removeById(id: ObjectId) =
-        usersCollection.deleteOne(eq(User::id.name, id)).wasAcknowledged()
+  override suspend fun removeById(id: ObjectId) =
+    usersCollection.deleteOne(eq(User::id.name, id)).wasAcknowledged()
 
-    override suspend fun clear() {
-        usersCollection.drop()
-    }
+  override suspend fun clear() {
+    usersCollection.drop()
+  }
 }
